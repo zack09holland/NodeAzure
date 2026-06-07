b34451a18aaf1806bcfcbf50cf362ffd4b47b824
zack09holland <zack09holland@gmail.com>
2026-06-07 14:38:47 -0500

    fix: cropped header

--- server.js
+++ server.js
@@ -107,5 +107,5 @@ require("./app/routing/htmlRoutes")(app);
 // ==============================================================================
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+  console.log("App listening on http://localhost:" + PORT);
 });
